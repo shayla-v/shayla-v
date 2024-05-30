5bba83c8c1a4a0c8820faa65981aa4b4f52ca7a1
shayla-v <shayla.valdez64@gmail.com>
2024-05-29 23:01:48 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// Open the default tab
+document.getElementById("Education").style.display = "block";
+document.getElementsByClassName("tablink")[0].className += " active";
